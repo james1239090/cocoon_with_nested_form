9e9a7d90f2aad6b0995ef921f345fc317fbddd60
James <james1239090@gmail.com>
2017-05-03 15:50:54 +0800

    use the strong param with nested attributes forproduct

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:id,:name,
+                                    product_options_attributes:[:id,:color_id, :dimension_id,:_destroy,
+                                                                color_attributes:[:id,:name,:_destroy],
+                                                                dimension_attributes:[:id,:name,:_destroy]])
+  end
 end
