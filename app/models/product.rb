7d17e6ead8c73616ce1744e77908d7b2c25d9f78
James <james1239090@gmail.com>
2017-05-03 15:45:40 +0800

    setup relationship and nested_attributes for each model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  has_many :product_options, dependent: :destroy
+  has_many :dimensions, :through => :product_options, source: :dimension
+  has_many :colors, :through => :product_options, source: :color
+  accepts_nested_attributes_for :product_options, :allow_destroy => true
+  accepts_nested_attributes_for :dimensions, :allow_destroy => true
+  accepts_nested_attributes_for :colors, :allow_destroy => true
+
+  validates :name, presence: true
 end
