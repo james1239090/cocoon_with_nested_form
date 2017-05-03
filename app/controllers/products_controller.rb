09acaacf1fbf0de14efe9fb1e03945fe70d38770
James <james1239090@gmail.com>
2017-05-03 16:07:43 +0800

    implement destroy for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:edit,:update,:destroy]
   def index
     @products = Product.all
   end
@@ -16,13 +17,10 @@ class ProductsController < ApplicationController
     end
   end
 
-
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to products_path
     else
@@ -30,10 +28,18 @@ class ProductsController < ApplicationController
       puts "#{@product.errors.full_messages}"
       render :edit
     end
+  end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
   def product_params
     params.require(:product).permit(:id,:name,
                                     product_options_attributes:[:id,:color_id, :dimension_id,:_destroy,
