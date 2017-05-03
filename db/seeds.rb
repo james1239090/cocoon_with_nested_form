f42bdcb12a6dd897af27969382f0bbe1782bf4d8
James <james1239090@gmail.com>
2017-05-03 16:14:48 +0800

    add seed for deafult color and dimension

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Color.destroy_all
+Dimension.destroy_all
+
+Color.create(name:'Red')
+Color.create(name:'White')
+Color.create(name:'Black')
+Dimension.create(name:'M')
+Dimension.create(name:'L')
+Dimension.create(name:'XL')
